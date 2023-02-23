d259196c2966facc5ed1260c41657eb6a849bd97
AlexAR <moltron_nano@hotmail.com>
2023-02-23 17:07:31 -0600

    feat: add working along for identicon in elixir

---
+++ README.md
@@ -0,0 +1,22 @@
+# Identicon
+
+Image generator similar to github automatic user profile generation.
+Working along from:
+The Complete Elixir and Phoenix Bootcamp - Stephen Grider
+https://www.udemy.com/course/the-complete-elixir-and-phoenix-bootcamp-and-tutorial/
+
+
+"sample" generates:
+![Screenshot](sample.png)
+
+## To run
+
+```console
+$ mix deps.get
+$ iex.bat -S mix
+$ Identicon.main("somestring")
+```
+
+
+
+
---
+++ lib/identicon.ex
@@ -0,0 +1,87 @@
+defmodule Identicon do
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail ]} = image) do
+    #longer path
+    #pattern matching a struct, there is no dot notation
+    # %Identicon.Image{hex: hex_list} = image
+    # [r, g, b | _tail ] = hex_list
+
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid,  fn({code, _index}) ->
+      rem(code,2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  #No longer pattern matching with the entire image variable as its the final part
+  # (only care about color and pixels at this point)
+  #WARNING: Side effects are and mutable variables
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250,250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+
+end
